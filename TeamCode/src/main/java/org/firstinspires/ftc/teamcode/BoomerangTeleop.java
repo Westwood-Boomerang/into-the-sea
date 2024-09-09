cc9954d8f8e5107200a5629876556ba599043e8d
Perry <atharv.sondhi@gmail.com>
2024-09-09 17:33:44 -0500

    Added basic tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleop.java
@@ -1,2 +1,28 @@
-package org.firstinspires.ftc.teamcode.subsystems;public class BoomerangTeleop {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.subsystems.driveTrain;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+@TeleOp(name = "BoomerangTeleop")
+public class BoomerangTeleop extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        //init drivetrain
+        driveTrain DriveTrain = new driveTrain(hardwareMap, driveTrain.Reverse.RevLeft,  new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD)
+        ),(x) -> {return x;});
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                DriveTrain.update(-gamepad1.left_stick_y,gamepad1.left_stick_x,gamepad1.right_stick_x, gamepad1.start);
+                // code goes here!,
+            }
+        }
+    }
 }
