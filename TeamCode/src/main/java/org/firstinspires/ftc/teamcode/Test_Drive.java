82e5d55224ee3ba627d65c59fb99eaa7146baa21
Perry <atharv.sondhi@gmail.com>
2024-09-14 15:46:49 -0500

    idk anything anymore

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Drive.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.teamcode.subsystems.*;
+
+@TeleOp(name = "Test_Drive")
+public class Test_Drive extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        driveTrain myTrain = new driveTrain(hardwareMap, driveTrain.Reverse.RevLeft,new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.LEFT)
+        ),(x)->{return x*x*x/Math.abs(x);});
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+               myTrain.update(gamepad1.left_stick_y,gamepad1.left_stick_x,gamepad1.right_stick_x,false);
+            }
+        }
+    }
+}
