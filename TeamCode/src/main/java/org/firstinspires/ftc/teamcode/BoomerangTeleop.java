a146f3c791bea59f91b6c9a49448a71d315c8078
Aditya Rayapeddi <ishaansingh.atx@gmail.com>
2024-10-21 16:56:55 -0500

    working on tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleop.java
@@ -32,6 +32,8 @@ public class BoomerangTeleop extends LinearOpMode {
         );
         DcMotorEx arm = hardwareMap.get(DcMotorEx.class, "Arm");
         DcMotorEx slides = hardwareMap.get(DcMotorEx.class, "Slides");
+        arm.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         arm.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         slides.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         waitForStart();
@@ -56,64 +58,51 @@ public class BoomerangTeleop extends LinearOpMode {
             currentArmPos = arm.getCurrentPosition();
 
                 //programs A button for claw
-//                if (gamepad1.a){
-//                    if (!claw_up) {
+//               if (gamepad1.left_bumper){
 //                        claw.setPosition(1);
-//                        claw_up = true;
-//                    }
-//                    else{
+//               } else if (gamepad1.right_bumper) {
 //                        claw.setPosition(0);
-//                        claw_up = false;
 //                    }
-//                }
 
             //checks to see if the arm is up. Then brings it down or takes it down.
             //programs B button for arm
             if (gamepad1.b){
+                arm.setPower(0.015);
                 arm.setTargetPosition(5);
                 arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-
-                arm.setPower(0.015);
-                wrist.setPosition(1);
-                claw.setPosition(1);
-                arm_up = true;
+                //wrist.setPosition(1);
+//                claw.setPosition(1);
 //                        claw_up = true;
             }
 
             if  (gamepad1.left_bumper) {
-                arm.setTargetPosition(2);
                 arm.setPower(0.015);
+                arm.setTargetPosition(2);
+
             } else if (gamepad1.right_bumper) {
                 arm.setPower(-0.015);
                 arm.setTargetPosition(0);
             } else {
-                arm.setPower(0);
+                arm.setPower(0.015);
                 arm.setTargetPosition(currentArmPos);
             }
-            arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
             if (gamepad1.x) {
-                sample ++;
-
-                //arm.setDirection(DcMotorEx.Direction.REVERSE);
-                arm.setPower(0.015);
+                arm.setPower(-0.015);
+                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
                 arm.setTargetPosition(0);
                 //wrist.setPosition(0);
-                arm_up = false;
-            }
+              }
 
-            if (gamepad1.y){
+            if (gamepad1.dpad_up){
+                arm.setPower(0.015);
                 arm.setTargetPosition(5);
-                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-                arm.setPower(0.015);
-
-                arm_up = true;
             }
-            if (gamepad1.dpad_down) {
-                arm.setTargetPosition(0);
+            else if (gamepad1.dpad_down) {
                 arm.setPower(0.015);
+                arm.setTargetPosition(0);
+                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
-                arm_up = false;
             }
 
             if (gamepad1.right_trigger >= 0.3 && slides.getCurrentPosition() < 5) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -99,10 +99,10 @@ public class SampleMecanumDrive extends MecanumDrive {
                 DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
         imu.initialize(parameters);
 
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
-        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        leftRear = hardwareMap.get(DcMotorEx.class, "backLeft");
+        rightRear = hardwareMap.get(DcMotorEx.class, "backRight");
+        rightFront = hardwareMap.get(DcMotorEx.class, "frontRight");
 
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
 
