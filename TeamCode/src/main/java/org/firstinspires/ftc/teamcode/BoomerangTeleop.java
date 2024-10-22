64c86cd8cb16f5e8134e9e9bcbcf90393686ff60
Atharv Sondhi <atharv.sondhi@gmail.com>
2024-10-22 14:43:57 -0500

    Added macros

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleop.java
@@ -5,9 +5,11 @@ TODO: ADD CLAW ITEMS
 TODO: ADD FTC Dash so that we can do this on the spot so it is faster
 TODO: Get rid of my crappy subsystems
  */
+
 import com.acmerobotics.roadrunner.control.PIDFController;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
 import org.firstinspires.ftc.teamcode.subsystems.LinearSlide;
 import org.firstinspires.ftc.teamcode.subsystems.PIDFcontroller;
@@ -15,6 +17,7 @@ import org.firstinspires.ftc.teamcode.subsystems.PIDFcontroller;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -43,13 +46,13 @@ public class BoomerangTeleop extends LinearOpMode {
         );
         DcMotorEx arm = hardwareMap.get(DcMotorEx.class, "Arm");
         DcMotorEx slides = hardwareMap.get(DcMotorEx.class, "Slides");
-        PIDFcontroller armPidf = new PIDFcontroller(0, 0, 0, 0, 0, 10, 0.8);
-        LinearSlide armController = new LinearSlide(arm, new int []{ 0, -300 }, 0, armPidf);
-        arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        slides.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         arm.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         slides.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        arm.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(0, 0, 0, 0));
+        slides.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(0, 0, 0, 0));
+
         waitForStart();
+
 
         Servo claw = hardwareMap.get(Servo.class, "claw");
         Servo wrist = hardwareMap.get(Servo.class, "wrist");
@@ -69,29 +72,28 @@ public class BoomerangTeleop extends LinearOpMode {
             currGamepad.copy(gamepad1);
 
             driveTrain.update(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.start);
-            armController.update(gamepad1.left_bumper, gamepad1.right_bumper, gamepad1.a, gamepad1.b);
 
             currentSlidePos = slides.getCurrentPosition();
 
-                //programs A button for claw
-//               if (gamepad1.dpad_up){
-//                        claw.setPosition(1);
-//               } else if (gamepad1.dpad_down) {
-//                        claw.setPosition(0);
-//                    }
+            if (gamepad1.dpad_up) {
+                claw.setPosition(1);
+            } else if (gamepad1.dpad_down) {
+                claw.setPosition(0);
+            }
 
             //checks to see if the arm is up. Then brings it down or takes it down.
             //programs B button for arm
-            if (gamepad1.b){
-                arm.setTargetPosition(1);
-                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-                arm.setPower(0.015);
+            if (gamepad1.b) {
                 slides.setTargetPosition(5);
                 slides.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
                 slides.setPower(0.015);
-                //wrist.setPosition(1);
+                arm.setTargetPosition(1);
+                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                arm.setPower(0.015);
+//                wrist.setPosition(1);
 //                claw.setPosition(1);
             }
+            // Who knows what this does... idk
 
             if (gamepad1.x) {
                 slides.setTargetPosition(0);
@@ -103,8 +105,7 @@ public class BoomerangTeleop extends LinearOpMode {
                 //wrist.setPosition(0);
                 //arm.setPower(-0.015);
                 //currentArmPos = arm.getCurrentPosition();
-              }
-
+            }
 
 
             if (gamepad1.right_trigger >= 0.3 && slides.getCurrentPosition() < 5) {
