2883a0c31f1de695f4d5f9cf95e2ff76e65de5fe
adi <@boomerang>
2025-01-17 16:21:09 -0600

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleop.java
@@ -1,179 +1,179 @@
-package org.firstinspires.ftc.teamcode;
-/*
-horizontal claw = Servo Axon = horclaw
-horizontal slides extendo = Servo Axon = horext
-
- */
-
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.acmerobotics.roadrunner.control.PIDFController;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.Gamepad;
-
-import com.acmerobotics.dashboard.config.Config;
-
-
-@Config
-@TeleOp(name = "BoomerangTeleopNonFSM")
-@Disabled
-public class BoomerangTeleop extends LinearOpMode {
-    public static int maxSlidesPos = 4000;
-    public static int slideWallPos = 400;
-    public static int topBarSlidePos = 3085;
-    public static int topBarSlidePosDown = 2100;
-    @Override
-    public void runOpMode() {
-        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
-        //init drivetrain
-        DriveTrain driveTrain = new DriveTrain(hardwareMap,
-                new String[]{"frontRight", "frontLeft", "backRight", "backLeft"},
-                DriveTrain.Reverse.RevRight,
-                "imu",
-                new IMU.Parameters(
-                        new RevHubOrientationOnRobot(
-                                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
-                        )
-                ),
-                x -> x
-        );
-
-        DcMotorEx vert = hardwareMap.get(DcMotorEx.class, "Vert");
-        DcMotorEx vert2 = hardwareMap.get(DcMotorEx.class, "Vert2");
-        vert.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        vert2.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        //arm.setPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION, new PIDFCoefficients(0, 0, 0, 0));
-        //slides.setPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION, new PIDFCoefficients(0, 0, 0, 0));
-
-        waitForStart();
-
-        Servo extClaw = hardwareMap.get(Servo.class, "extClaw");
-        Servo wrist1 = hardwareMap.get(Servo.class, "wrist1");
-        Servo bucket1 = hardwareMap.get(Servo.class, "bucket1");
-        Servo bucket2 = hardwareMap.get(Servo.class, "bucket2");
-//      Servo wrist2 = hardwareMap.get(Servo.class, "wrist2");
-        Servo ext1 = hardwareMap.get(Servo.class, "ext1");
-        Servo ext2 = hardwareMap.get(Servo.class, "ext2");
-        Servo vertClaw = hardwareMap.get(Servo.class, "vertClaw");
-
-
-
-        vert.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        vert2.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        vert.setDirection(DcMotorSimple.Direction.REVERSE);
-        vert2.setDirection(DcMotorSimple.Direction.REVERSE);
-
-
-        int targetVertPos = 0;
-        double UpPower = 0.4;
-        double DownPower = 0.1;
-        double CurrPower = 0.0;
-        String extendoState = "bucket";
-        vert.setTargetPosition(targetVertPos);
-        vert.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        vert2.setTargetPosition(targetVertPos);
-        vert2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        vert.setPower(1);
-        vert2.setPower(1);
-
-        while (opModeIsActive()) {
-            telemetry.addData("slide Pos", vert.getCurrentPosition());
-            telemetry.addData("slide pos 2", vert2.getCurrentPosition());
-            telemetry.addData("slide Pow", vert.getPower());
-            telemetry.addData("targetSlides", vert.getTargetPosition());
-            telemetry.addData("extendoState", extendoState);
-            telemetry.update();
-
-            driveTrain.update(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.start);
-            if (gamepad1.right_bumper) {
-                // close
-                vertClaw.setPosition(0.8);
-            } else if (gamepad1.left_bumper) {
-                // open
-                vertClaw.setPosition(0.2);
-            }
-
-            //checks to see if the arm is up. Then brings it down or takes it down.
-            //programs B button for arm
-            if (gamepad1.b) {
-                // takes it to the side wall height
-                targetVertPos = slideWallPos;
-            } else if (gamepad1.a) {
-                // just above top bar
-                targetVertPos = topBarSlidePos;
-                vertClaw.setPosition(0.8);
-            } else if (gamepad1.x) {
-                // below top bar, goes down & lets go of spec
-                targetVertPos = topBarSlidePosDown;
-                vertClaw.setPosition(0.2);
-            } else if (gamepad1.y) {
-                // bottom
-                targetVertPos = 0;
-            }
-
-            if (gamepad1.right_trigger >= 0.3) {
-                // TODO: figure out what max slide position is
-                targetVertPos = Math.min(vert.getCurrentPosition() + 200, maxSlidesPos);
-                CurrPower = UpPower; // TODO: Someone correct me if I am wrong
-            } else if (gamepad1.left_trigger >= 0.3) {
-                targetVertPos = Math.max(vert.getCurrentPosition() - 200, 0);
-                CurrPower = DownPower;
-            }
-
-            //TODO: fix this up
-            if (gamepad1.dpad_up && !extendoState.equals("extending") && !extendoState.equals("extended")) {
-                extendoState = "extending";
-                ext1.setPosition(1);
-                ext2.setPosition(1);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    return;
-                }
-                wrist1.setPosition(1);
-                extendoState = "extended";
-            } else if (gamepad1.dpad_down && !extendoState.equals("comingBack") && !extendoState.equals("bucket")) {
-                extendoState = "comingBack";
-                extClaw.setPosition(0);
-                wrist1.setPosition(0);
-                ext1.setPosition(0);
-                ext2.setPosition(0);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    return;
-                }
-                extClaw.setPosition(1);
-                extendoState = "bucket";
-
-            }
-            if (gamepad1.dpad_left) {
-                bucket1.setPosition(0);
-                bucket2.setPosition(0);
-            } else if (gamepad1.dpad_right) {
-                bucket1.setPosition(1);
-                bucket2.setPosition(1);
-            }
-            vert.setTargetPosition(targetVertPos);
-            vert2.setTargetPosition(targetVertPos);
-        }
-    }
-}
-
-
+//package org.firstinspires.ftc.teamcode;
+///*
+//horizontal claw = Servo Axon = horclaw
+//horizontal slides extendo = Servo Axon = horext
+//
+// */
+//
+//import com.acmerobotics.dashboard.FtcDashboard;
+//import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+//import com.acmerobotics.roadrunner.control.PIDFController;
+//import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import org.firstinspires.ftc.robotcore.external.Telemetry;
+//import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorEx;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+//import com.qualcomm.robotcore.hardware.Servo;
+//import com.qualcomm.robotcore.hardware.IMU;
+//import com.qualcomm.robotcore.hardware.Gamepad;
+//
+//import com.acmerobotics.dashboard.config.Config;
+//
+//
+//@Config
+//@TeleOp(name = "BoomerangTeleopNonFSM")
+//@Disabled
+//public class BoomerangTeleop extends LinearOpMode {
+//    public static int maxSlidesPos = 4000;
+//    public static int slideWallPos = 400;
+//    public static int topBarSlidePos = 3085;
+//    public static int topBarSlidePosDown = 2100;
+//    @Override
+//    public void runOpMode() {
+//        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+//        //init drivetrain
+//        DriveTrain driveTrain = new DriveTrain(hardwareMap,
+//                new String[]{"frontRight", "frontLeft", "backRight", "backLeft"},
+//                DriveTrain.Reverse.RevRight,
+//                "imu",
+//                new IMU.Parameters(
+//                        new RevHubOrientationOnRobot(
+//                                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+//                                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+//                        )
+//                ),
+//                x -> x
+//        );
+//
+//        DcMotorEx vert = hardwareMap.get(DcMotorEx.class, "Vert");
+//        DcMotorEx vert2 = hardwareMap.get(DcMotorEx.class, "Vert2");
+//        vert.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+//        vert2.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+//        //arm.setPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION, new PIDFCoefficients(0, 0, 0, 0));
+//        //slides.setPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION, new PIDFCoefficients(0, 0, 0, 0));
+//
+//        waitForStart();
+//
+//        Servo extClaw = hardwareMap.get(Servo.class, "extClaw");
+//        Servo wrist1 = hardwareMap.get(Servo.class, "wrist1");
+//        Servo bucket1 = hardwareMap.get(Servo.class, "bucket1");
+//        Servo bucket2 = hardwareMap.get(Servo.class, "bucket2");
+////      Servo wrist2 = hardwareMap.get(Servo.class, "wrist2");
+//        Servo ext1 = hardwareMap.get(Servo.class, "ext1");
+//        Servo ext2 = hardwareMap.get(Servo.class, "ext2");
+//        Servo vertClaw = hardwareMap.get(Servo.class, "vertClaw");
+//
+//
+//
+//        vert.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+//        vert2.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+//        vert.setDirection(DcMotorSimple.Direction.REVERSE);
+//        vert2.setDirection(DcMotorSimple.Direction.REVERSE);
+//
+//
+//        int targetVertPos = 0;
+//        double UpPower = 0.4;
+//        double DownPower = 0.1;
+//        double CurrPower = 0.0;
+//        String extendoState = "bucket";
+//        vert.setTargetPosition(targetVertPos);
+//        vert.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        vert2.setTargetPosition(targetVertPos);
+//        vert2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        vert.setPower(1);
+//        vert2.setPower(1);
+//
+//        while (opModeIsActive()) {
+//            telemetry.addData("slide Pos", vert.getCurrentPosition());
+//            telemetry.addData("slide pos 2", vert2.getCurrentPosition());
+//            telemetry.addData("slide Pow", vert.getPower());
+//            telemetry.addData("targetSlides", vert.getTargetPosition());
+//            telemetry.addData("extendoState", extendoState);
+//            telemetry.update();
+//
+//            driveTrain.update(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.start);
+//            if (gamepad1.right_bumper) {
+//                // close
+//                vertClaw.setPosition(0.8);
+//            } else if (gamepad1.left_bumper) {
+//                // open
+//                vertClaw.setPosition(0.2);
+//            }
+//
+//            //checks to see if the arm is up. Then brings it down or takes it down.
+//            //programs B button for arm
+//            if (gamepad1.b) {
+//                // takes it to the side wall height
+//                targetVertPos = slideWallPos;
+//            } else if (gamepad1.a) {
+//                // just above top bar
+//                targetVertPos = topBarSlidePos;
+//                vertClaw.setPosition(0.8);
+//            } else if (gamepad1.x) {
+//                // below top bar, goes down & lets go of spec
+//                targetVertPos = topBarSlidePosDown;
+//                vertClaw.setPosition(0.2);
+//            } else if (gamepad1.y) {
+//                // bottom
+//                targetVertPos = 0;
+//            }
+//
+//            if (gamepad1.right_trigger >= 0.3) {
+//                // TODO: figure out what max slide position is
+//                targetVertPos = Math.min(vert.getCurrentPosition() + 200, maxSlidesPos);
+//                CurrPower = UpPower; // TODO: Someone correct me if I am wrong
+//            } else if (gamepad1.left_trigger >= 0.3) {
+//                targetVertPos = Math.max(vert.getCurrentPosition() - 200, 0);
+//                CurrPower = DownPower;
+//            }
+//
+//            //TODO: fix this up
+//            if (gamepad1.dpad_up && !extendoState.equals("extending") && !extendoState.equals("extended")) {
+//                extendoState = "extending";
+//                ext1.setPosition(1);
+//                ext2.setPosition(1);
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    Thread.currentThread().interrupt();
+//                    return;
+//                }
+//                wrist1.setPosition(1);
+//                extendoState = "extended";
+//            } else if (gamepad1.dpad_down && !extendoState.equals("comingBack") && !extendoState.equals("bucket")) {
+//                extendoState = "comingBack";
+//                extClaw.setPosition(0);
+//                wrist1.setPosition(0);
+//                ext1.setPosition(0);
+//                ext2.setPosition(0);
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    Thread.currentThread().interrupt();
+//                    return;
+//                }
+//                extClaw.setPosition(1);
+//                extendoState = "bucket";
+//
+//            }
+//            if (gamepad1.dpad_left) {
+//                bucket1.setPosition(0);
+//                bucket2.setPosition(0);
+//            } else if (gamepad1.dpad_right) {
+//                bucket1.setPosition(1);
+//                bucket2.setPosition(1);
+//            }
+//            vert.setTargetPosition(targetVertPos);
+//            vert2.setTargetPosition(targetVertPos);
+//        }
+//    }
+//}
+//
+//
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tickBasedRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tickBasedRed.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-import java.util.*;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
@@ -7,14 +6,12 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.util.Encoder;
 
 @Config
 @Autonomous(preselectTeleOp = "BoomerangTeleopOpMode")
@@ -25,9 +22,9 @@ public class tickBasedRed extends LinearOpMode {
 //    public static double blpos = 2.9562;
 //    public static double brpos = -6.2285;
     public static double flpos = -1.5;
-    public static double frpos = -1.5;
+    public static double frpos = -1.3;
     public static double blpos = -1.5;
-    public static double brpos = -1.5;
+    public static double brpos = -1.3;
     DcMotorEx fl;
     DcMotorEx fr;
     DcMotorEx bl;
@@ -87,27 +84,29 @@ public class tickBasedRed extends LinearOpMode {
             //slides up
             drive(0, 0, 0, 0, 1700, false, 1000);
             //go back for spec 1
-//            drive(-1.5, -1.5, -1.5, -1.5, 1700, false, 2000);
-            drive(flpos, frpos, blpos, brpos, 1700, true, 3000);
+            drive(-1.5, -1.5, -1.5, -1.5, 1700, false, 2000);
             //slides go down to clip spec 1
-            drive(0, 0, 0, 0, 1300, true, 3000);
+            drive(0, 0, 0, 0, 1300, true, 1000);
             //go right to get spec
-            drive(-3, 3, 3, -3, 300, true, 3000);
+            drive(-3.5, 3.5, 3.5, -3.5, 300, true, 3000);
             //spin 180 degrees
             claw.setPosition(openPos);
             drive(2.5, -2.5, 2.5, -2.5, 300, false, 3000);
             //go to back wall to pick up spec
+            drive(-0.8, -0.8, -0.8, -0.8, 300, true, 3000);
             claw.setPosition(closePos);
-            drive(-1.05, -1.05, -1.05, -1.05, 300, false, 3000);
             //strafe left
-            drive(-2.0, 2.0, 2.0, -2.0, 1700, false, 3000);
+            drive(-2.5, 2.5, 2.5, -2.5, 1700, false, 3000);
             //go backwards toward hang
             drive(0.75, 0.75, 0.75, 0.75, 1700, false, 3000);
             //turn 180 to have vert claw face the submersible
-            drive(-2.3, 2.3, -2.3, 2.3, 1700, false, 3000);
+            drive(-2.4, 2.4, -2.4, 2.4, 1700, false, 3000);
+            //go backwards toward submersible (again)
+            drive(-0.8, -0.8, -0.8, -0.8, 1800, false, 3000);
             //slides go down to place
-            drive(0, 0, 0, 0, 1300, true, 3000);
-            drive(0, 0, 0, 0, 0, true, 100);
+            drive(0, 0, 0, 0, 1350, true, 3000);
+            claw.setPosition(openPos);
+            drive(0, 0, 0, 0, 0, true, 1000);
         }
     }
 
@@ -120,18 +119,18 @@ public class tickBasedRed extends LinearOpMode {
 
         while (opModeIsActive() && time.milliseconds() < t)
         {
-            fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            vert.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            vert2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             fl.setTargetPosition((int) (tpr * flpos));
             fr.setTargetPosition((int) (tpr * frpos));
             bl.setTargetPosition((int) (tpr * blpos));
             br.setTargetPosition((int) (tpr * brpos));
             vert.setTargetPosition(slidePos);
             vert2.setTargetPosition(slidePos);
+            fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            vert.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            vert2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             fl.setPower(0.75);
             fr.setPower(0.75);
@@ -156,7 +155,10 @@ public class tickBasedRed extends LinearOpMode {
         } else {
             claw.setPosition(1);
         }
-
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         fl.setPower(0);
         fr.setPower(0);
         bl.setPower(0);
